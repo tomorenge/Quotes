2737c85d61004f356f0ccf14e3feef8a6d17b1c0
Tomorenge <tomorenge3@gmail.com>
2019-03-25 07:57:48 +0300

    added a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
