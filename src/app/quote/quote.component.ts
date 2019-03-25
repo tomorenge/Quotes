55bc2fca2557b1e2e27a26ec018c0e1c01fb6665
Tomorenge <tomorenge3@gmail.com>
2019-03-25 18:13:06 +0300

    changed the quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,9 +11,9 @@ export class QuoteComponent implements OnInit {
   title = "Welcome to Quotes"
 
   quotes = [
-  new Quote(1,"lorem ipsum","Sir Newton","leo",0,0,new Date(2019,2,23)),
-  new Quote(2,"lorem ipsumtwo","Sir Newton", "kevo",0,0,new Date()),
-  new Quote(3,"lorem ipsumthree", "Sir Newton", "Tom",0,0, new Date())
+  new Quote(1,"Let the people mask you such that you become one with the crowd.","Al muamin","leo",0,0,new Date(2019,2,23)),
+  new Quote(2,"Do we bend the rules in service to a greater good? And if we do, what does it say of us?","Altaïr's Codex", "kevo",0,0,new Date()),
+  new Quote(3,"For if nothing is true, then why believe anything? And if everything is permitted... why not chase every desire?", "Edward Kenway to the Mentor Ah Tabai, 1722", "Tom",0,0, new Date())
 ]
 toggleDetail(index){
   this.quotes[index].showDescription=!this.quotes[index].showDescription
--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote} from '../quote'
 
 @Component({
   selector: 'app-vote',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
-  constructor() { }
+  @Output() isComplete= newEventEmitter<boolean>();
+
+  upVotes=0;
+  downVotes=0;
+
+  upVote(){
+
+    this.upVotes=this.upVotes+1
+  }
+
+  downVote(){
+    this.downVotes=this.downVotes+1
+  }
+
+  constructor() {}
 
   ngOnInit() {
   }
